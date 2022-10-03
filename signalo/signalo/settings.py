ca9f905fd75791eda45e08fccf9c2578fba3f294
Denis Rouzaud <denis.rouzaud@gmail.com>
2022-10-03 18:51:31 +0200

    yeaah

--- signalo/signalo/settings.py
+++ signalo/signalo/settings.py
@@ -122,7 +122,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = os.getenv("DJANGO_STATIC_ROOT", BASE_DIR.joinpath("static"))
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.getenv("DJANGO_MEDIA_ROOT", BASE_DIR.joinpath("media"))
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
