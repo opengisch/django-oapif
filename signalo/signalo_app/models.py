e1a8ac0bd3ee9031887e3087f200b85213a22b77
Denis Rouzaud <denis.rouzaud@gmail.com>
2022-10-03 17:36:14 +0200

    first

---
+++ signalo/signalo_app/models.py
@@ -0,0 +1,17 @@
+from django.contrib.gis.db import models
+from django.utils.translation import gettext as _
+import uuid
+
+
+class Pole(models.Model):
+    #id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    geom = models.PointField(srid=2056, verbose_name=_("Geometry"))
+    name = models.CharField(max_length=255, verbose_name=_("Name"))
+
+
+class Sign(models.Model):
+    #id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    pole = models.ForeignKey(
+        Pole, on_delete=models.CASCADE, blank=True, null=True, related_name="poles"
+    )
+    #order = models.IntegerField( default=1)
