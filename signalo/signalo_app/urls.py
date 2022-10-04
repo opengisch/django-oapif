f026268c8b15b16cad61db50d17a8a185dcece19
Damiano Lombardi <damiano@opengis.ch>
2022-10-04 16:28:51 +0200

    rest framework gis requirement

--- signalo/signalo_app/urls.py
+++ signalo/signalo_app/urls.py
@@ -1,13 +1,12 @@
+from django.urls import path
 from django_wfs3.urls import wfs3_router
-from django.urls import path, include
-from signalo_app.views import SignViewset, PoleViewset
+from signalo_app.views import PoleViewset, SignViewset
+
 from . import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("wfs3/", include(wfs3_router.urls)),  # Django-rest urls
-]
-
 
 wfs3_router.register(r"signs", SignViewset, "sign")
 wfs3_router.register(r"poles", PoleViewset, "pole")
+
+urlpatterns = [
+    path("", views.index, name="index"),
+]
