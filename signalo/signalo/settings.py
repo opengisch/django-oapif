1455842bd6c9f2d4842379b025e328e3c1ae6500
Denis Rouzaud <denis.rouzaud@gmail.com>
2022-10-04 17:57:11 +0200

    geom computed field

--- signalo/signalo/settings.py
+++ signalo/signalo/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.gis",
     "rest_framework",
     "rest_framework_gis",
+    "computedfields",
 ]
 
 MIDDLEWARE = [
--- signalo/signalo_app/models.py
+++ signalo/signalo_app/models.py
@@ -1,17 +1,26 @@
-from django.contrib.gis.db import models
-from django.utils.translation import gettext as _
 import uuid
 
+from computedfields.models import ComputedFieldsModel, computed
+from django.contrib.gis.db import models
+from django.utils.translation import gettext as _
 
-class Pole(models.Model):
+
+class Pole(ComputedFieldsModel):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     geom = models.PointField(srid=2056, verbose_name=_("Geometry"))
     name = models.CharField(max_length=255, verbose_name=_("Name"))
 
 
-class Sign(models.Model):
+class Sign(ComputedFieldsModel):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     pole = models.ForeignKey(
-        Pole, on_delete=models.CASCADE, blank=True, null=True, related_name="poles"
+        Pole, on_delete=models.CASCADE, blank=True, null=True, related_name="signs"
     )
-    order = models.IntegerField( default=1)
+    order = models.IntegerField(default=1)
+
+    @computed(
+        models.PointField(srid=2056, verbose_name=_("Geometry")),
+        depends=[("self", ["pole"]), ("pole", ["geom"])],
+    )
+    def computed_geom(self):
+        return self.pole.geom
