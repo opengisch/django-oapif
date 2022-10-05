0a4b9d1fd0cac884d34c1b0540440db879417d89
Damiano Lombardi <damiano@opengis.ch>
2022-10-05 13:59:56 +0200

    Fix pagination links

--- signalo/django_oapif/pagination.py
+++ signalo/django_oapif/pagination.py
@@ -6,10 +6,20 @@ class CustomPagination(pagination.LimitOffsetPagination):
     def get_paginated_response(self, data):
         return Response(
             {
-                "links": {
-                    "next": self.get_next_link(),
-                    "previous": self.get_previous_link(),
-                },
+                "links": [
+                    {
+                        "type": "application/geo+json",
+                        "rel": "next",
+                        "title": "items (next)",
+                        "href": self.get_next_link(),
+                    },
+                    {
+                        "type": "application/geo+json",
+                        "rel": "previous",
+                        "title": "items (previous)",
+                        "href": self.get_previous_link(),
+                    },
+                ],
                 "numberReturned": len(data["features"]),
                 "numberMatched": self.count,
                 **data,
