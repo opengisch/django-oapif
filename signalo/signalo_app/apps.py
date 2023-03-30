839da30de8c913791676150c4e44a6b3374e0870
Adrien <mrnycticorax@gmail.com>
2023-03-30 12:46:16 +0200

    Updated precommit hooks and applied (#6)

--- signalo/signalo_app/apps.py
+++ signalo/signalo_app/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class SignaloAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'signalo_app'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "signalo_app"
