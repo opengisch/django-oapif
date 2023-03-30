20038f741c94d7a5bea4f4bfadb7360a04aa5011
Olivier Dalang <olivier.dalang@gmail.com>
2023-03-30 17:07:09 +0200

    fix pagination issue (#14)

--- src/django_oapif/pagination.py
+++ src/django_oapif/pagination.py
@@ -4,51 +4,58 @@ from rest_framework.response import Response
 
 
 class OapifPagination(pagination.LimitOffsetPagination):
+    """OAPIF-compatible django rest paginator"""
+
     def get_paginated_response(self, data):
-        if type(data) == dict:
-            return Response(
-                {
-                    "links": [
-                        {
-                            "type": "application/geo+json",
-                            "rel": "next",
-                            "title": "items (next)",
-                            "href": self.get_next_link(),
-                        },
-                        {
-                            "type": "application/geo+json",
-                            "rel": "previous",
-                            "title": "items (previous)",
-                            "href": self.get_previous_link(),
-                        },
-                    ],
-                    "numberReturned": len(data["features"]),
-                    "numberMatched": self.count,
-                    **data,
-                }
-            )
+        return Response(
+            {
+                "links": [
+                    {
+                        "type": "application/geo+json",
+                        "rel": "next",
+                        "title": "items (next)",
+                        "href": self.get_next_link(),
+                    },
+                    {
+                        "type": "application/geo+json",
+                        "rel": "previous",
+                        "title": "items (previous)",
+                        "href": self.get_previous_link(),
+                    },
+                ],
+                "numberReturned": len(data["features"]),
+                "numberMatched": self.count,
+                **data,
+            }
+        )
 
-        else:
-            number_returned = len(data)
-            data = f'"type": "FeatureCollection", "features": [{",".join(data)}]'
 
-            return HttpResponse(
-                f"""{{
-                    "links": [
-                        {{
-                            "type": "application/geo+json",
-                            "rel": "next",
-                            "title": "items (next)",
-                            "href": "{self.get_next_link()}"
-                        }},
-                        {{
-                            "type": "application/geo+json",
-                            "rel": "previous",
-                            "title": "items (previous)",
-                            "href": "{self.get_previous_link()}"
-                        }}
-                    ],
-                    "numberReturned": {number_returned},
-                    "numberMatched": {self.count},
-                    {data}}}"""
-            )
+class HighPerfPagination(pagination.LimitOffsetPagination):
+    """OAPIF-compatible django rest paginator, tailored for the high performance version where data is pre-concatenated json"""
+
+    def get_paginated_response(self, data):
+        # FIXME: this probably is a bug, since `data` is a string, it is not the number of features
+        number_returned = len(data)
+        data = f'"type": "FeatureCollection", "features": [{",".join(data)}]'
+
+        return HttpResponse(
+            f"""{{
+                "links": [
+                    {{
+                        "type": "application/geo+json",
+                        "rel": "next",
+                        "title": "items (next)",
+                        "href": "{self.get_next_link()}"
+                    }},
+                    {{
+                        "type": "application/geo+json",
+                        "rel": "previous",
+                        "title": "items (previous)",
+                        "href": "{self.get_previous_link()}"
+                    }}
+                ],
+                "numberReturned": {number_returned},
+                "numberMatched": {self.count},
+                {data}}}
+            """
+        )
--- src/signalo/core/views.py
+++ src/signalo/core/views.py
@@ -5,6 +5,7 @@ from rest_framework import viewsets
 from rest_framework_gis.serializers import GeoFeatureModelSerializer
 
 from django_oapif.mixins import OAPIFDescribeModelViewSetMixin
+from django_oapif.pagination import HighPerfPagination
 
 from .models import Pole, Sign
 
@@ -23,8 +24,8 @@ class PoleSerializer(GeoFeatureModelSerializer):
     # used only for API route
     class Meta:
         model = Pole
-        fields = "__all__"
         geo_field = "geom"
+        exclude = ["_serialized"]
 
 
 class SignSerializer(GeoFeatureModelSerializer):
@@ -62,6 +63,7 @@ class PoleViewset(OAPIFDescribeModelViewSetMixin, viewsets.ModelViewSet):
 
 class PoleHighPerfViewset(OAPIFDescribeModelViewSetMixin, viewsets.ModelViewSet):
     serializer_class = PoleSerializer
+    pagination_class = HighPerfPagination
     oapif_title = "Poles High Performance"
     oapif_description = "Poles layer - high performance"
     # (one day this will be retrieved automatically from the serializer)
@@ -84,6 +86,7 @@ class PoleHighPerfViewset(OAPIFDescribeModelViewSetMixin, viewsets.ModelViewSet)
 
         for pole in queryset:
             serialized_poles.append(pole or "")
+
         return OapifResponse(serialized_poles)
 
     def get_queryset(self):
