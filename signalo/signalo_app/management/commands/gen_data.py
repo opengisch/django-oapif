0a01825adffe6a8cc17270a00b8c5e7df0aed353
why-not-try-calmer <mrnycticorax@gmail.com>
2023-03-30 13:52:40 +0200

    Fixing imports

--- signalo/signalo_app/management/commands/gen_data.py
+++ signalo/signalo_app/management/commands/gen_data.py
@@ -1,7 +1,8 @@
-from django.core.management.base import BaseCommand
 from django.core.management import call_command
+from django.core.management.base import BaseCommand
 from django.db import transaction
-from signalo_app.models import Pole, Sign
+
+from signalo.core.models import Pole, Sign
 
 
 class Command(BaseCommand):
