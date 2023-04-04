da052f80126358b87dd757c468234ce50ea8ed72
why-not-try-calmer <mrnycticorax@gmail.com>
2023-04-04 11:24:20 +0200

    ---

--- conformance-tests/ogcf.py
+++ conformance-tests/ogcf.py
@@ -4,34 +4,37 @@ from urllib.parse import quote_plus, urlencode
 import requests
 
 
+def is_200(res: requests.Response) -> bool:
+    return res.status_code == 200
+
+
 class ConformanceTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        app_service = "django:8000"
-        conformance_service = "conformance_suite"
-        cls.api_url = f"http://{app_service}/oapif/"
-        cls.test_client_url = f"http://{conformance_service}/teamengine/rest/suites/ogcapi-features-1.0/run"
+        app_address = "django:8000"
+        cls.api_url = f"http://{app_address}/oapif/"
+        
+        conformance_suite_address = "conformance_suite:8081"
+        cls.conformance_suites_url = f"http://{conformance_suite_address}/teamengine/rest/suites"
+        cls.conformance_ogc_suite_url = f"{cls.conformance_suites_url}/ogcapi-features-1.0/run"
 
-    def test_api_endpoint(self):
+    def test_endpoint_django(self):
         print(f"Trying to access: {self.api_url}")
         response = requests.get(self.api_url)
-        condition = response.status_code == 200
+        self.assertTrue(is_200(response))
 
-        if not condition:
-            print(response.status_code)
-        self.assertTrue(condition)
+    def test_endpoint_testsuite(self):
+        print(f"Trying to access: {self.conformance_suites_url}")
+        response = requests.get(self.conformance_suites_url)
+        self.assertTrue(is_200(response))
 
-    def test_api_endpoint_conformance(self):
-        params = {"iut": self.api_url}
-        encoded_params = urlencode(params, quote_via=quote_plus)
-        url = f"{self.test_client_url}?{encoded_params}"
-        headers = {"Accept": "application/xml"}
-
-        print(f"Validating: {self.api_url} with {url}")
-        response = requests.get(url, headers=headers)
-
-        condition = response.status_code == 200
-        self.assertTrue(condition)
+    # def test_conformance_api(self):
+    #     params = {"iut": self.api_url}
+    #     encoded_params = urlencode(params, quote_via=quote_plus)
+    #     url = f"{self.test_client_url}?{encoded_params}"
+    #     headers = {"Accept": "application/xml"}
+    #     response = requests.get(url, headers=headers)
+    #     self.assertTrue(is_200(response))
 
 
 if __name__ == "__main__":
