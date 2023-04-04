2d7a7200da16a9b156ce511c1105b977d6c2a2f4
why-not-try-calmer <mrnycticorax@gmail.com>
2023-04-04 10:23:45 +0200

    Accomodating comments

--- conformance-tests/ogcf.py
+++ conformance-tests/ogcf.py
@@ -7,12 +7,12 @@ import requests
 class ConformanceTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        app_service = "django"
+        app_service = "django:8000"
         conformance_service = "conformance_suite"
         cls.api_url = f"http://{app_service}/oapif/"
         cls.test_client_url = f"http://{conformance_service}/teamengine/rest/suites/ogcapi-features-1.0/run"
 
-    def test_url(self):
+    def test_api_endpoint(self):
         print(f"Trying to access: {self.api_url}")
         response = requests.get(self.api_url)
         condition = response.status_code == 200
@@ -21,7 +21,7 @@ class ConformanceTest(unittest.TestCase):
             print(response.status_code)
         self.assertTrue(condition)
 
-    def test_endpoint(self):
+    def test_api_endpoint_conformance(self):
         params = {"iut": self.api_url}
         encoded_params = urlencode(params, quote_via=quote_plus)
         url = f"{self.test_client_url}?{encoded_params}"
