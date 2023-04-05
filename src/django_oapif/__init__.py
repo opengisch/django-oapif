704357940e86da57c066f732c454cfdf4d34bcf9
Olivier Dalang <olivier.dalang@gmail.com>
2023-04-05 14:07:24 +0200

    store version in code

--- src/django_oapif/__init__.py
+++ src/django_oapif/__init__.py
@@ -1,1 +1,5 @@
-__version__ = "0.0.0"  # DO NOT CHANGE THIS LINE - it will be replaced by CI workflow
+try:
+    from .__version__ import __version__, __version_tuple__
+except ImportError:
+    __version__ = "0.0.0.dev"
+    __version_tuple__ = (0, 0, 0, "dev")
