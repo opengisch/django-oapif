ca644f9054982ce14455733d6f235947b88b5ebb
why-not-try-calmer <mrnycticorax@gmail.com>
2023-04-18 14:41:57 +0200

    Added 'value_lists' app

---
+++ src/signalo/value_lists/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class SignaloAppConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "signalo.value_lists"
+    label = "signalo_vl"
