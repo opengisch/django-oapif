320ab79cb8b496f8bd812924a7e0c8bc5910d3fa
Adrien <mrnycticorax@gmail.com>
2023-04-21 12:21:23 +0200

    pole <- 1:n -> azimuth <- 1:n -> sign

--- src/signalo/core/models.py
+++ src/signalo/core/models.py
@@ -6,6 +6,7 @@ from django.contrib.gis.db import models
 from django.utils.translation import gettext as _
 
 from django_oapif.decorators import register_oapif_viewset
+from signalo.value_lists.models import OfficialSignType
 
 
 @register_oapif_viewset()
@@ -23,12 +24,12 @@ class Pole(ComputedFieldsModel):
 
 
 @register_oapif_viewset()
-class Sign(ComputedFieldsModel):
+class Azimuth(ComputedFieldsModel):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    value = models.SmallIntegerField(default=0, null=False, blank=False)
     pole = models.ForeignKey(
-        Pole, on_delete=models.CASCADE, blank=True, null=True, related_name="signs"
+        Pole, on_delete=models.CASCADE, blank=True, null=True, related_name="azimuths"
     )
-    order = models.IntegerField(default=1)
 
     @computed(
         models.PointField(
@@ -40,3 +41,37 @@ class Sign(ComputedFieldsModel):
     )
     def geom(self):
         return self.pole.geom
+
+
+@register_oapif_viewset()
+class Sign(ComputedFieldsModel):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    sign_type = models.ForeignKey(
+        OfficialSignType,
+        models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name="official_sign",
+    )
+    azimuth = models.ForeignKey(
+        Azimuth,
+        models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name="signs",
+    )
+    order = models.IntegerField(null=False, blank=False)
+
+    @computed(
+        models.PointField(
+            srid=settings.GEOMETRY_SRID,
+            verbose_name=_("Geometry"),
+            null=True,
+        ),
+        depends=[
+            ("self", ["azimuth"]),
+            ("azimuth", ["geom"]),
+        ],
+    )
+    def geom(self):
+        return self.geom
