99dd788c906acbac067d211c50fd7a1e2348aa4c
Adrien <mrnycticorax@gmail.com>
2023-04-28 09:43:30 +0200

    Update src/signalo/urls.py
    Co-authored-by: Ivan Ivanov <suricactus@users.noreply.github.com>

--- src/signalo/urls.py
+++ src/signalo/urls.py
@@ -21,6 +21,6 @@ from django_oapif.urls import oapif_router
 urlpatterns = [
     path("signalo_core/", include("signalo.core.urls")),
     path("admin/", admin.site.urls),
-    path("oapif/", include(oapif_router.urls)),  # Django-rest urls,
+    path("oapif/", include(oapif_router.urls)),  # Django-rest urls
     path("__debug__/", include("debug_toolbar.urls")),  # Debug toolbar
 ]
