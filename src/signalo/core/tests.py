2de83772a42e09666d65e292d8f6c63059d70d00
Adrien <mrnycticorax@gmail.com>
2023-05-01 16:41:33 +0200

    Added gprof2dot visualization to artifacts

--- src/signalo/core/tests.py
+++ src/signalo/core/tests.py
@@ -109,11 +109,11 @@ def serialize_with_profile(
 class SpeedTestSerialization(APITestCase):
     @classmethod
     def setUpClass(cls, *args, **kwargs):
+        super().setUpClass(*args, **kwargs)
         call_command("populate_vl")
         call_command("populate_data", magnitude=100)
         cls.poles = Pole.objects.all()
         cls.path = os.path.abspath("/unit_tests_outputs")
-        super().setUpClass(*args, **kwargs)
 
     def test_data(self):
         self.assertEqual(self.poles.count(), 10000)
