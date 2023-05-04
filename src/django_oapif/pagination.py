ca4caee4344fbaf0a60f7816d31781dccf0d7a54
Adrien <mrnycticorax@gmail.com>
2023-05-04 13:07:38 +0200

    Fixing pagination for non-geometrical features; removing dead code from settings

--- src/django_oapif/pagination.py
+++ src/django_oapif/pagination.py
@@ -1,3 +1,5 @@
+from typing import Dict
+
 from django.http import HttpResponse
 from rest_framework import pagination
 from rest_framework.response import Response
@@ -7,6 +9,8 @@ class OapifPagination(pagination.LimitOffsetPagination):
     """OAPIF-compatible django rest paginator"""
 
     def get_paginated_response(self, data):
+        # Some items might non-geometrical features. We need to distinguish
+        numberReturned = len(data["features"]) if isinstance(data, Dict) else len(data)
         return Response(
             {
                 "links": [
@@ -23,9 +27,9 @@ class OapifPagination(pagination.LimitOffsetPagination):
                         "href": self.get_previous_link(),
                     },
                 ],
-                "numberReturned": len(data["features"]),
+                "numberReturned": numberReturned,
                 "numberMatched": self.count,
-                **data,
+                "data": data,
             }
         )
 
