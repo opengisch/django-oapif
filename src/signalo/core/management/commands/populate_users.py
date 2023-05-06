71ab0bebc47717d0dbb1cc4d53621dbe117fe961
why-not-try-calmer <mrnycticorax@gmail.com>
2023-05-06 11:44:58 +0200

    ...

--- src/signalo/core/management/commands/populate_users.py
+++ src/signalo/core/management/commands/populate_users.py
@@ -29,12 +29,14 @@ class Command(BaseCommand):
         editors.permissions.set(editing)
         viewers.permissions.set(viewing)
 
-        an_editor, _ = User.objects.get_or_create(
-            username="demo_editor", password="123"
+        a_viewer, _ = User.objects.get_or_create(
+            username="demo_viewer", password="viewer_password"
         )
-        a_viewer, _ = User.objects.get_or_create(username="demo_viewer", password="123")
+        an_editor, _ = User.objects.get_or_create(
+            username="demo_editor", password="editor_password"
+        )
         a_super_user = User.objects.create_superuser(
-            username="admin", password="123", is_staff=True
+            username="admin", password="admin_password", is_staff=True
         )
 
         a_viewer.save()
