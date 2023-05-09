248e02443ae61e41f8a02ad364085a76065a7a5f
Adrien <mrnycticorax@gmail.com>
2023-05-09 15:28:48 +0200

    Publishing to PyPI (#72)
    * Publishing to PyPI
    Various small fixes
    Create LICENSE
    No test on tags; any tag.
    All tags and Python 3.9
    prepare_package.sh
    Cleaned up
    Path
    token=password
    verbosity flag
    0.1.6 as fallback
    * Refreshed documentation
    * Removed unnecessary condition from workflow

---
+++ scripts/prepare_then_build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+mkdir django-ogcapif
+cp -r src/django_oapif django-ogcapif/django_ogcapif/
+cp pyproject.toml LICENSE README.md requirements.txt requirements-dev.txt \
+    django-ogcapif/
+cd django-ogcapif
+python -m build
