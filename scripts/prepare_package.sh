737cb98236e72e3950fc53cbfad3bc1dbbf8a90f
Adrien <mrnycticorax@gmail.com>
2023-05-09 09:50:54 +0200

    Cleaned up

--- scripts/prepare_package.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-mkdir django-ogcapif
-cp -r src/django_oapif django-ogcapif/django_ogcapif/
-cp MANIFEST.in pyproject.toml LICENSE README.md requirements.txt requirements-dev.txt django-ogcapif/
---
+++ scripts/prepare_then_build.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+mkdir django-ogcapif
+cp -r src/django_oapif django-ogcapif/django_ogcapif/
+cp MANIFEST.in pyproject.toml LICENSE README.md requirements.txt requirements-dev.txt \
+    django-ogcapif/
+python -m build django-ogcapif/
