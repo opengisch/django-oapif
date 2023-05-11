287977d7ee57a4cbbecab60c1ccf1e0392eb4779
Adrien <mrnycticorax@gmail.com>
2023-05-11 12:05:48 +0200

    Property

--- src/signalo/roads/management/commands/populate_roads.py
+++ src/signalo/roads/management/commands/populate_roads.py
@@ -10,12 +10,13 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         roads_csv = path.relpath("./signalo/roads/data/roads.csv")
-        roads = []
         with open(roads_csv, "r") as fh:
             next(fh)
+            roads = []
             for line in fh:
-                geom, _ = line.split(",", 1)
-                road = Road(geom=bytes(geom, encoding="utf-8"))
-                roads.append(road)
+                geom, _ = line.rsplit(",", 1)
+                geom = geom.strip('""')
+                roads.append(Road(geom=geom))
+
         Road.objects.bulk_create(roads)
         print(f"🛣️ Added roads!")
--- src/signalo/roads/models.py
+++ src/signalo/roads/models.py
@@ -1,12 +1,19 @@
 import uuid
+from typing import Union
 
-from django.db import models
-from django.db.models import fields
+from django.contrib.gis.db import models
+from django.utils.translation import gettext as _
 
 from django_oapif.decorators import register_oapif_viewset
+from signalo.settings import GEOMETRY_SRID
 
 
 @register_oapif_viewset()
 class Road(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    geom = fields.BinaryField()
+    geom = models.MultiLineStringField(srid=GEOMETRY_SRID, verbose_name=_("Geometry"))
+
+    def get_geom(self, geometry: str = "wkb") -> Union[memoryview, str]:
+        if geometry == "wkb":
+            return self.geom.wkb
+        return self.geom.wkt
