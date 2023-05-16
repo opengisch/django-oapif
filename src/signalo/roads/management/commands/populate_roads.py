40bcdcc38d2e6216a81a81717243e4edba78aafc
Denis Rouzaud <denis.rouzaud@gmail.com>
2023-05-16 21:46:26 +0200

    test roads (#77)
    * test roads
    * 'populate_roads' now creates roads in batches of 1000 to use less RAM
    ---------
    
    Co-authored-by: Adrien <mrnycticorax@gmail.com>

--- src/signalo/roads/management/commands/populate_roads.py
+++ src/signalo/roads/management/commands/populate_roads.py
@@ -1,8 +1,24 @@
 from os import path
+from typing import Generator, Iterable, List
 
 from django.core.management.base import BaseCommand
 
 from ...models import Road
+
+
+def make_roads_batches(fh: Iterable) -> Generator[List[Road], None, None]:
+    roads = []
+    counter = 0
+
+    for line in fh:
+        geom, _ = line.rsplit(",", 1)
+        geom = geom.strip('""')
+        roads.append(Road(geom=geom))
+        counter += 1
+
+    if counter == 1000 or next(fh, None) is None:
+        yield roads
+        counter = 0
 
 
 class Command(BaseCommand):
@@ -12,11 +28,8 @@ class Command(BaseCommand):
         roads_csv = path.relpath("./signalo/roads/data/roads.csv")
         with open(roads_csv, "r") as fh:
             next(fh)
-            roads = []
-            for line in fh:
-                geom, _ = line.rsplit(",", 1)
-                geom = geom.strip('""')
-                roads.append(Road(geom=geom))
-
-        Road.objects.bulk_create(roads)
-        print(f"🛣️ Added roads!")
+            total = 0
+            for batch in make_roads_batches(fh):
+                Road.objects.bulk_create(batch)
+            total += len(batch)
+        print(f"🛣️ Added {total} roads!")
