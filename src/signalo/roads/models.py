bc6ebd48aa8e7e2b4f300f0f85961e457c1ca90e
Adrien <mrnycticorax@gmail.com>
2023-05-23 11:20:42 +0200

    Added custom pagination for roads (#79)

--- src/signalo/roads/models.py
+++ src/signalo/roads/models.py
@@ -3,12 +3,17 @@ from typing import Union
 
 from django.contrib.gis.db import models
 from django.utils.translation import gettext as _
+from rest_framework import pagination
 
 from django_oapif.decorators import register_oapif_viewset
 from signalo.settings import GEOMETRY_SRID
 
 
-@register_oapif_viewset()
+class Limit1000(pagination.LimitOffsetPagination):
+    default_limit = 1000
+
+
+@register_oapif_viewset(custom_viewset_attrs={"pagination_class": Limit1000})
 class Road(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     geom = models.MultiLineStringField(srid=GEOMETRY_SRID, verbose_name=_("Geometry"))
