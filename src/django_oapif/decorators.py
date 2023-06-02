e17c242ca7fb7402a71f11b6d897485d4cb42557
Denis Rouzaud <denis.rouzaud@gmail.com>
2023-06-02 08:21:34 +0200

    move code to crs_utils (#84)
    * move code to crs_utils
    
    * typo

--- src/django_oapif/decorators.py
+++ src/django_oapif/decorators.py
@@ -1,4 +1,3 @@
-import re
 from os import getenv
 from typing import Any, Callable, Dict, Optional
 
@@ -12,60 +11,8 @@ from rest_framework_gis.serializers import GeoFeatureModelSerializer
 from django_oapif.mixins import OAPIFDescribeModelViewSetMixin
 from django_oapif.urls import oapif_router
 
+from .crs_utils import get_crs_from_uri
 from .filters import BboxFilterBackend
-
-# taken from https://github.com/geopython/pygeoapi/blob/953b6fa74d2ce292d8f566c4f4d3bcb4161d6e95/pygeoapi/util.py#L90
-CRS_AUTHORITY = [
-    "AUTO",
-    "EPSG",
-    "OGC",
-]
-
-CRS_URI_PATTERN = re.compile(
-    (
-        rf"^http://www.opengis\.net/def/crs/"
-        rf"(?P<auth>{'|'.join(CRS_AUTHORITY)})/"
-        rf"[\d|\.]+?/(?P<code>\w+?)$"
-    )
-)
-
-
-# taken from
-def get_crs_from_uri(uri: str) -> CRS:
-    """
-    Get a `pyproj.CRS` instance from a CRS URI.
-    Author: @MTachon
-
-    :param uri: Uniform resource identifier of the coordinate
-        reference system.
-    :type uri: str
-
-    :raises `CRSError`: Error raised if no CRS could be identified from the
-        URI.
-
-    :returns: `pyproj.CRS` instance matching the input URI.
-    :rtype: `pyproj.CRS`
-    """
-
-    try:
-        crs = CRS.from_authority(*CRS_URI_PATTERN.search(uri).groups())
-    except RuntimeError:
-        msg = (
-            f"CRS could not be identified from URI {uri!r} "
-            f"(Authority: {CRS_URI_PATTERN.search(uri).group('auth')!r}, "
-            f"Code: {CRS_URI_PATTERN.search(uri).group('code')!r})."
-        )
-        raise RuntimeError(msg)
-    except AttributeError:
-        msg = (
-            f"CRS could not be identified from URI {uri!r}. CRS URIs must "
-            "follow the format "
-            "'http://www.opengis.net/def/crs/{authority}/{version}/{code}' "
-            "(see https://docs.opengeospatial.org/is/18-058r1/18-058r1.html#crs-overview)."  # noqa
-        )
-        raise AttributeError(msg)
-    else:
-        return crs
 
 
 def register_oapif_viewset(
