f824e5712252adae6131a44bdda8dd853960a337
Denis Rouzaud <denis.rouzaud@gmail.com>
2023-10-06 14:37:41 +0200

    Update functions.py

--- src/django_oapif/functions.py
+++ src/django_oapif/functions.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # see https://code.djangoproject.com/ticket/34882#ticket
 # and https://github.com/django/django/pull/17320
-
+# this will be fixed in Django 5.0
 
 class AsGeoJSON(functions.AsGeoJSON):
     output_field = models.TextField()
