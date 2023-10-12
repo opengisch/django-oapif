8db628a21fee4fff63099f9f18e26a43788916b4
Denis Rouzaud <denis.rouzaud@gmail.com>
2023-10-12 16:14:27 +0200

    debug

--- tests/benchmark/time.sh
+++ tests/benchmark/time.sh
@@ -8,6 +8,7 @@ LAYERS=( point_2056_10fields point_2056_10fields_local_geom nogeom_10fields noge
 for SIZE in "${SIZES[@]}"; do
   echo "::group::setup ${SIZE}"
   ./scripts/restart.sh ${SIZE}
+  sleep 2
   echo "::endgroup::"
 
   LIMIT=10
@@ -16,6 +17,7 @@ for SIZE in "${SIZES[@]}"; do
 
     for LAYER in "${LAYERS[@]}"; do
       hyperfine -r 10 "curl http://${OGCAPIF_HOST}:${DJANGO_DEV_PORT}/oapif/collections/tests.${LAYER}/items?limit=${LIMIT}" --export-json .time.json
+      cat .time.json
       echo "$SIZE $LIMIT $LAYER $(cat .time.json | jq '.results.[0].mean')" >> benchmark.dat
     done
   done
