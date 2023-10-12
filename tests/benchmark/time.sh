410489e32fd5efb74bf2876c3fc9927b01a03e8a
Denis Rouzaud <denis.rouzaud@gmail.com>
2023-10-12 09:17:46 +0200

    move code

--- tests/benchmark/time.sh
+++ tests/benchmark/time.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 
-LIMIT=${1}
+SIZES=( 100 1000 10000 )
+for SIZE in "${SIZES[@]}"; do
+  echo "::group::setup ${SIZE}"
+  ./scripts/restart.sh ${SIZE}
+  echo "::endgroup::"
 
-hyperfine -r 10 "curl http://0.0.0.0:7180/oapif/collections/tests.point_2056_10fields/items?limit=${LIMIT}"
+  LIMIT=10
+  while [[ $LIMIT -lt $SIZE ]]; do
+    LIMIT=$((LIMIT*10))
 
-hyperfine -r 10 "curl http://0.0.0.0:7180/oapif/collections/tests.point_2056_10fields_local_json/items?limit=${LIMIT}"
+  hyperfine -r 10 "curl http://0.0.0.0:7180/oapif/collections/tests.point_2056_10fields/items?limit=${LIMIT}"
+  hyperfine -r 10 "curl http://0.0.0.0:7180/oapif/collections/tests.point_2056_10fields_local_json/items?limit=${LIMIT}"
+
+  done
+done
+
