62bcd6e42ae1302a9ae9f7afadb42939ceb2ee81
Denis Rouzaud <denis.rouzaud@gmail.com>
2023-10-13 16:59:56 +0200

    retry

--- tests/benchmark/time.sh
+++ tests/benchmark/time.sh
@@ -7,7 +7,7 @@ OUTPUT_PATH="tests/benchmark/results"
 mkdir -p ${OUTPUT_PATH}
 rm -f ${OUTPUT_PATH}/benchmark.dat
 
-SIZES=( 100 1000 10000 100000 )
+SIZES=( 100 1000 ) #10000 100000 )
 LAYERS=( point_2056_10fields point_2056_10fields_local_geom nogeom_10fields nogeom_100fields line_2056_10fields line_2056_10fields_local_geom polygon_2056_10fields polygon_2056_10fields_local_geom secretlayer )
 
 for SIZE in "${SIZES[@]}"; do
