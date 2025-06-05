cb72d8af704ba24ec249d3089989e46a6ce4e1ef
Aurèle Ferotin <aurele.ferotin@icloud.com>
2025-06-05 08:45:47 +0200

    Update conformances

--- django_oapif/conformance.py
+++ django_oapif/conformance.py
@@ -11,8 +11,15 @@ def create_conformance_router():
     def conformance(request: HttpRequest):
         return OAPIFConformance(
             conformsTo=[
+                "http://www.opengis.net/spec/ogcapi-common-1/1.0/conf/core",
+                "http://www.opengis.net/spec/ogcapi-common-1/1.0/conf/html",
+                "http://www.opengis.net/spec/ogcapi-common-1/1.0/conf/json",
+                "http://www.opengis.net/spec/ogcapi-common-1/1.0/conf/landing-page",
+                "http://www.opengis.net/spec/ogcapi-common-1/1.0/conf/oas30",
+                "http://www.opengis.net/spec/ogcapi-common-2/1.0/conf/collections",
                 "http://www.opengis.net/spec/ogcapi-features-1/1.0/conf/core",
                 "http://www.opengis.net/spec/ogcapi-features-1/1.0/conf/geojson",
+                "http://www.opengis.net/spec/ogcapi-features-1/1.0/req/oas30",
                 "http://www.opengis.net/spec/ogcapi-features-1/1.0/req/oas31",
                 "http://www.opengis.net/spec/ogcapi-features-2/1.0/conf/crs",
                 "http://www.opengis.net/spec/ogcapi-features-4/1.0/req/create-replace-delete",
