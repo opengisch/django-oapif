301c6d208e2ffd108702111c537cd6a6ca105750
Denis Rouzaud <denis.rouzaud@gmail.com>
2026-09-24 13:13:37 +0200

    Point the WSGI and ASGI apps at the settings module that exists

--- tests/django_oapif_tests/tests/wsgi.py
+++ tests/django_oapif_tests/tests/wsgi.py
@@ -10,6 +10,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_oapif_tests.tests.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_oapif_tests.settings")
 
 application = get_wsgi_application()
